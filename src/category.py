bad9b4524f59185949b1aeea7256ad843534de81
lutovinova_mariya <bukovka09@gmail.com>
2026-07-09 23:24:25 +0300

    добавить метод подсчета среднего ценника всех товаров

--- src/category.py
+++ src/category.py
@@ -8,6 +8,7 @@ class Category:
     product_count = 0
 
     def __init__(self, name: str, description: str, products: list = None) -> None:
+        """Инициализация категории продуктов."""
         self.name = name
         self.description = description
         self.__products = []
@@ -41,3 +42,14 @@ class Category:
     def products_list(self) -> list:
         """Возвращает прямой список объектов продуктов для обратной совместимости."""
         return self.__products
+
+    def average_price(self) -> float:
+        """Подсчитывает средний ценник всех товаров в категории.
+
+        Возвращает 0, если в категории нет товаров.
+        """
+        try:
+            total_price = sum(product.price for product in self.__products)
+            return total_price / len(self.__products)
+        except ZeroDivisionError:
+            return 0
